62f3caa4340651f31a61c472c2cf3214e786f239
francesco <savaresef@gmail.com>
2024-12-30 00:12:01 +0900

    dpcs

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 /**
  * This function is used as a test function
- * Super Extra test
+ * Super Super Extra test
  * @brief multiply the input number per 2 and return it 
  * @param number to multiply
  * @return The number multiplied per 2
