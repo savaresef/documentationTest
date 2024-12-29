f32009bfb9ccc912994d3cda94683e8ec8cd172a
francesco <savaresef@gmail.com>
2024-12-29 19:01:04 +0900

    added cmake

---
+++ build/CMakeFiles/documentationTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/documentationTest.dir/src/main.cpp.o"
+  "documentationTest"
+  "documentationTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/documentationTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
