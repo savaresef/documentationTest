de9ea53b087516c3b2c1d797c9ba2aab04ecd5d8
francesco <savaresef@gmail.com>
2024-12-29 19:18:13 +0900

    created workflow action

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+/**
+ * @brief multiply the input number per 2 and return it 
+ * @param int number to multiply
+ * @return The number multiplied per 2
+ */
+int printNumber(int number);
 int main() {
-    std::cout << "Hello, CMake!" << std::endl;
+    std::cout << "Enter a new num:" << std::endl;
+    int num;
+    std::cin >> num;
+    std::cout << printNumber(num) <<std::endl;
     return 0;
+}
+
+int printNumber(int number)
+{
+    return number * 2;
 }
